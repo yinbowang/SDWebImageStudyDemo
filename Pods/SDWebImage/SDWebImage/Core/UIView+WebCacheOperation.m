e258a36b7c9f4ab5a9cf7169fb5be7b1f4ea14db
wyb <389621905@qq.com>
2019-08-29 22:32:44 +0800

    #import "UIView+WebCacheOperation.h"

--- Pods/SDWebImage/SDWebImage/Core/UIView+WebCacheOperation.m
+++ Pods/SDWebImage/SDWebImage/Core/UIView+WebCacheOperation.m
@@ -17,18 +17,24 @@ typedef NSMapTable<NSString *, id<SDWebImageOperation>> SDOperationsDictionary;
 
 @implementation UIView (WebCacheOperation)
 
+//获取任务字典NSMapTable,将NSMapTable关联对象到UIView
 - (SDOperationsDictionary *)sd_operationDictionary {
     @synchronized(self) {
+        //如果取到operations就返回否则就创建,保证线程安全
         SDOperationsDictionary *operations = objc_getAssociatedObject(self, &loadOperationKey);
         if (operations) {
             return operations;
         }
+        //https://www.jianshu.com/p/cf4e15b26f64
+        //NSHashTable与NSMapTable
+        //用NSMapTable保存operation,NSMapTable可以设置value为弱引用,key为强引用
         operations = [[NSMapTable alloc] initWithKeyOptions:NSPointerFunctionsStrongMemory valueOptions:NSPointerFunctionsWeakMemory capacity:0];
         objc_setAssociatedObject(self, &loadOperationKey, operations, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
         return operations;
     }
 }
 
+//通过key去拿operation
 - (nullable id<SDWebImageOperation>)sd_imageLoadOperationForKey:(nullable NSString *)key  {
     id<SDWebImageOperation> operation;
     if (key) {
@@ -40,9 +46,12 @@ typedef NSMapTable<NSString *, id<SDWebImageOperation>> SDOperationsDictionary;
     return operation;
 }
 
+//设置key和value
 - (void)sd_setImageLoadOperation:(nullable id<SDWebImageOperation>)operation forKey:(nullable NSString *)key {
     if (key) {
+        //先把之前的key对应的operation给删除了
         [self sd_cancelImageLoadOperationWithKey:key];
+        //然后添加新的operation
         if (operation) {
             SDOperationsDictionary *operationDictionary = [self sd_operationDictionary];
             @synchronized (self) {
@@ -52,6 +61,7 @@ typedef NSMapTable<NSString *, id<SDWebImageOperation>> SDOperationsDictionary;
     }
 }
 
+//通过key把operation给取出来,然后调用cancel方法,取消下载
 - (void)sd_cancelImageLoadOperationWithKey:(nullable NSString *)key {
     if (key) {
         // Cancel in progress downloader from queue
